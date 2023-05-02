839c1623a16d5c526fbe37205f00b1752c4e5bb1
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-05-02 13:33:01 -0400

    changed the root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
     def hello 
-        render html: "hello, world!"
+        render html: "hola, mundo!"
+    end
+    def goodbye
+        render html: "goodbye, world"
     end
 end
